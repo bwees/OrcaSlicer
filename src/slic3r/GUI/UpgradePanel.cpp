82df3e005e0e3841975fcb5c65088e593ba44591
bwees <brandonwees@gmail.com>
2026-05-22 09:05:34 -0500

    chore: revert background colors on upload pane

--- src/slic3r/GUI/UpgradePanel.cpp
+++ src/slic3r/GUI/UpgradePanel.cpp
@@ -1612,7 +1612,6 @@ bool UpgradePanel::Show(bool show)
      const wxString& name /*= wxEmptyString*/)
      : wxPanel(parent, id, pos, size, style)
  {
-     SetBackgroundColour(*wxWHITE);
      upgrade_green_icon = ScalableBitmap(this, "monitor_upgrade_online", 5);
 
      auto top_sizer = new wxBoxSizer(wxVERTICAL);
